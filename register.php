00a5866afcf11e454de72bbb82d8d9f2541f1ebd
xyzoptooo <dannydophlix@gmail.com>
2025-07-17 18:23:11 +0300

    1.0.2

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+require_once "dbconnect.php";
+
+$fullnames = trim($_POST['fullnames']);
+$username = trim($_POST['username']);
+$password = md5($_POST['password']);  // For security use password_hash in real apps
+$email = trim($_POST['email']);
+
+$check = mysqli_query($myconn, "SELECT * FROM users WHERE username='$username'");
+if (mysqli_num_rows($check) > 0) {
+    echo "<h1>Error</h1><p>Username already exists. Please choose another.</p>";
+} else {
+    $insert = mysqli_query($myconn, "INSERT INTO users (fullnames, username, password, email)
+                VALUES ('$fullnames', '$username', '$password', '$email')");
+    if ($insert) {
+        echo "<h1>Success</h1><p>Account created. <a href='login.html'>Login here</a></p>";
+    } else {
+        echo "<h1>Error</h1><p>Registration failed. Try again.</p>";
+    }
+}
+?>
