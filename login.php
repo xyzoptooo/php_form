562725c9e816ed26ede0be3d9adf76181066a509
xyzoptooo <dannydophlix@gmail.com>
2025-07-17 18:33:03 +0300

    1.0.2

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once "dbconnect.php";
+
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+
+$result = mysqli_query($myconn, "SELECT * FROM users WHERE username='$username' AND password='$password'");
+if (mysqli_num_rows($result) == 1) {
+    $_SESSION['username'] = $username;
+    echo "<h1>Welcome, $username</h1>";
+    echo "<a href='orderform.php'>Place Order</a> | <a href='view_tickets.php'>View Tickets</a>";
+} else {
+    echo "<h1>Error</h1><p>Invalid credentials. <a href='login.html'>Try again</a></p>";
+}
+?>
